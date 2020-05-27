84f1559b9f9bbeca7851a560bbbe9421f898f9bb
Borsuk <hubert.lichaczewski@gmail.com>
2020-05-27 12:39:16 +0200

    Initial relase

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+	[SerializeField]
+	private GameObject ground; //gameObject Of ground
+	[SerializeField]
+	private  GameObject ball; //GameObject of Ball
+	[SerializeField]
+	private GameObject trajectoryDotPrefab; //Prefab of trajectory Dot
+	[SerializeField]
+	private float xSpeed = 0.1f; //How fast x will grow
+	[SerializeField]
+	private float ySpeed = 0.18f; //How fast y will grow
+	[SerializeField]
+	private float maxX = 12.5f; //sets maximum value of x so it doesnt go over bounds
+	[SerializeField]
+	private float maxY = 19.2f; //sets maximum value of y so it doesnt go over bounds
+	private List<GameObject> GeneratedDots = new List<GameObject>(); //list of generated dots - used to destroy them
+
+  	private readonly Vector2 gravity = new Vector2(0f, -10f); //gravity used to calculate dots
+  	private Vector2 launchVector = new Vector2(0,0); //Vector to give ball after mouse goes up
+  	private Vector2 startPos; //Starting position for dots is determined by ball starting position
+	private Rigidbody2D rigidBody;
+	private float x = 0.5f; //x of vector to launch
+	private float y = 0.5f; //y of vector to launch
+	private int howManyDots = 5; //how many dots should be shown
+	private int i = 0; //iterator used for determining value of howManyDots
+	private bool mouseOnBall = false; // checks if mouse is on ball
+	private bool inHole = false; //checks if the ball hit the hole
+	private bool launched = false; //check if player already launched the ball
+	private bool nextLevel = false; //initiates loading of next level
+	private bool over = false;
+
+	public bool isOver(){ 
+		return over;
+	}
+
+	public bool isNext(){
+		return nextLevel;
+	}
+
+		void OnMouseDown(){ //when mouse is clicked on ball 
+		mouseOnBall = true;
+	}
+	void OnMouseUp(){ //when mouse stops clicking on ball
+		mouseOnBall = false;
+		if(!launched){
+			Launch();
+		}else{
+			DestroyDots();
+		}
+	}
+	void Awake () { 
+		rigidBody = GetComponent<Rigidbody2D> ();
+		startPos = ball.transform.position; //use ball position to set starting position
+	}
+	
+	void Update() {
+		i++;
+		if(Input.GetMouseButton(0)&&!launched&&mouseOnBall){
+			if(x>=maxX || y>=maxY){
+				Launch();
+			}
+			x = x + xSpeed;
+			y = y + ySpeed;
+			launchVector.x = x;
+			launchVector.y = y;
+			if(i%5 == 0){ 
+				howManyDots++; //increases value of dots shown every 5 iterarions
+			}
+			DrawDots(howManyDots,0.2f);
+		}
+
+		if(rigidBody.velocity == new Vector2(0,0)&&inHole){ //waits until ball stops in hole
+			nextLevel = true;
+		}
+
+		if(rigidBody.velocity == new Vector2(0,0)&&!inHole&&launched){ //waits until ball stops outside hole
+			over = true;
+		}
+	}
+
+	public void RestartBall(){ //restarts ball position and the variables
+		if(over == true){
+			xSpeed = 0.2f;
+			ySpeed = 0.18f;
+		}else{
+			xSpeed = xSpeed + 0.05f;
+			ySpeed = ySpeed + 0.05f;
+		}
+		x = 0.5f;
+		y = 0.5f;
+		launched = false;
+		inHole = false;
+		nextLevel = false;
+		over = false;
+		ball.transform.position = startPos;
+		Physics2D.IgnoreCollision(ground.GetComponent<Collider2D>(),GetComponent<Collider2D>(),false);
+	}
+
+
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.tag == "PotHole"){
+			//Debug.Log("Entered Collision with pothole!");
+			Physics2D.IgnoreCollision(ground.GetComponent<Collider2D>(),GetComponent<Collider2D>()); //when ball triggers pot hole, disable collisions with ground
+		 }
+	}
+
+	void OnCollisionStay2D(Collision2D col){
+		if(col.gameObject.tag == "PotHoleDown"){
+				inHole = true;
+			}else{
+				inHole = false;
+			}
+		
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if(col.gameObject.tag == "PotHole"){
+			//Debug.Log("Exited Collision with pothole!");
+			Physics2D.IgnoreCollision(ground.GetComponent<Collider2D>(),GetComponent<Collider2D>(),false); //when ball escapes from pot hole area, trigger collisions with ground on
+		 }
+	}
+
+  	private void Launch(){ //launches ball with given vector
+	  	DestroyDots();
+		launched = true;
+    	rigidBody.velocity = launchVector;
+  	}
+
+	private Vector2 CalculatePosition(float elapsedTime){  //calculates position of the ball after given elapsedTime 
+    	return gravity * elapsedTime * elapsedTime * 0.5f +
+               launchVector * elapsedTime + startPos;
+  	}
+
+	private void DrawDots(int howMany, float timeStep){//draws trajectory dots 
+		DestroyDots();
+		for (int i = 0; i < howMany; i++) {
+			GameObject trajectoryDot = Instantiate (trajectoryDotPrefab);
+			GeneratedDots.Add(trajectoryDot);
+    		trajectoryDot.transform.position = CalculatePosition (timeStep * i);
+		}
+	}
+
+	private void DestroyDots(){ //destroys all trajectory dots
+		foreach(var dot in GeneratedDots){
+			Destroy(dot);
+		}
+	}
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	[SerializeField]
+	private Button restartBut; //Restart button shown on game over screen
+	[SerializeField]
+	private BallController ballControl; //BallController link
+	[SerializeField]
+	private GameObject potHolePrefab; //Prefab of pothole
+	[SerializeField]
+	private float maxX = -9f; //maximum position of pothole to right
+	[SerializeField]
+	private float minX = 4.92f; //minimum position of pothole to left
+	[SerializeField]
+	private Text scoreText; //Text field with score
+	[SerializeField]
+	private Text scoreOverText; //Text field with current score on game over screen
+	[SerializeField]
+	private Text bestScoreText; //Text field with best score on game over screen
+	[SerializeField]
+	private GameObject gameOverScreen; //game over screen
+
+ 	private GameObject potHole; //saves pot hole game object
+	private int bestscore = 0; //keeps best score
+	private int score = 0; //keeps current score
+	
+
+	void Start () {
+		restartBut.onClick.AddListener(clickedRestart);
+		placePothole();
+		scoreText.text = "Score: "+score;
+	}
+	
+	void clickedRestart(){ //when restart button is clicked
+		score = 0;
+		scoreText.text = "Score: "+score;
+		nextLevel();
+	}
+
+	void showGameOver(){ //shows game over screen with scores
+		if(score > bestscore){
+			bestscore = score;
+		}
+		gameOverScreen.SetActive(true);
+
+		bestScoreText.text = "Best score: " + bestscore;
+		scoreOverText.text = "Current Score: "+score;
+	}
+
+	void placePothole(){ //randomly places pot hole
+		potHole = Instantiate (potHolePrefab);
+    	potHole.transform.position = new Vector2(Random.Range(minX,maxX),-0.02f);
+	}
+
+	void restartGame(){ //restarts current scene - SPACE
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
+	}
+
+	void nextLevel(){ //loads next level
+		gameOverScreen.SetActive(false);
+		Destroy(potHole);
+		ballControl.RestartBall();
+		placePothole();
+	}
+
+	void Update(){
+		
+		if(Input.GetKeyDown(KeyCode.Space)){
+			restartGame();
+		}
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Application.Quit();
+		}
+
+		if(ballControl.isNext() == true){
+			score++;
+			scoreText.text = "Score: "+score;
+			nextLevel();
+		}
+
+		if(ballControl.isOver() == true){
+			showGameOver();
+		}
+
+	}
+}
+
+
+
+
+
